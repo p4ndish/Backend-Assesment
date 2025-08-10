758d9144c5ce950aa1f75ebfc7d71d8f69a6f0a3
Dagim <tesfayedagim5@gmail.com>
2025-08-10 15:46:11 +0300

    feat: Implement StorageController with methods for user image retrieval and storage

---
+++ app/Http/Controllers/StorageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StorageController extends Controller
+{
+    public function getUserImage(Request $request)
+    {
+        // TODO: Implement getUserImage logic
+        return response()->json(['message' => 'getUserImage method not implemented yet']);
+    }
+
+    public function storeUserImage(Request $request)
+    {
+        // TODO: Implement storeUserImage logic
+        return response()->json(['message' => 'storeUserImage method not implemented yet']);
+    }
+} 
